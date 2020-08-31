c5ef9d58ecda8c13bed9c5a028245f02dd17d785
master-spectra <63547718+master-spectra@users.noreply.github.com>
2020-08-31 12:10:59 +0300

    Mega upgrade

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+$('.carusel').slick({
+	arrows: false,
+	dots: true
+});
+
+$('.profiles').slick({
+	arrows: false,
+	dots: true
+});
+
+$('.tab-container').tabPlugin({
+	speed: 300,
+	transition: 'linear'
+});
+
